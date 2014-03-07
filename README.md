2253acc3985f918da3c7afce8a50f650093f7591
Chris Mather <mather.chris@gmail.com>
2014-03-06 19:16:55 -0800

    initial commit.

---
+++ README.md
@@ -0,0 +1,42 @@
+### Synopsis
+UI.emboxValue runs its inner function one more time even if the dependent has
+been stopped. This happens when you use UI.With.
+
+### Reproduction
+
+1. Open JavaScript console and run with release/blaze-rc0.
+2. You should see "Template.two: options helper called" in the console.
+3. Click the "Toggle Template" button.
+4. In the console you should see the CHANGE DATA message followed by:
+  4a. Template.one: options helper called
+  4b. Template.two: options helper called
+
+### Why
+
+Clicking the toggle button does two things:
+
+1. Set a Session value that causes the rendered template to change.
+2. Calls `dataDep.changed()` which invalidates the global data function.
+
+Both templates' data functions call this global data function. When we call
+`dataDep.changed()` any dependent computations will be invalidated.
+
+When we write `{{#with options}}` in the template, it compiles to something like
+this:
+
+```javascript
+var render = function () {
+
+  // The first argument to UI.With will become an
+  // emboxedValue and that becomes the data property of
+  // the returned component.
+
+  return UI.With(function () {
+    // this lookup calls our global data function, creating
+    // the dependency
+    return Spacebars.call(self.lookup('options'));
+  }, UI.block(function () {
+    return content;
+  });
+};
+```
---
+++ embox.js
@@ -0,0 +1,86 @@
+if (Meteor.isClient) {
+  dataDep = new Deps.Dependency;
+
+  UI.body.events({
+    'click #toggle': function (e, tmpl) {
+      var value = Deps.nonreactive(function () {
+        return Session.get('showFirstTemplate');
+      });
+
+      // toggle the template
+      Session.set('showFirstTemplate', !value);
+
+
+      // now let's say that we set the session
+      // value above, but now we invalidate
+      // a computation that uses the emboxed
+      // args which would be the template data
+      // for example.
+
+      // the problem here is that the emboxed
+      // arg will run one more time before realizing
+      // it doesn't have any more dependencies and
+      // stopping itself.
+      //
+      // where did the emboxedArg collect this dependency?
+      // when the options function was called with the data context as the
+      // thisArg in Component.lookup.
+      //
+      // but it's only with emboxedArgs which get created with the UI.With
+      // helper. If you're not using emboxedArg it's not an issue.
+
+      console.log('%c**********CHANGE DATA**************', 'color: #ccc');
+      dataDep.changed();
+    }
+  });
+
+  Template.dynamicTemplates.showFirstTemplate = function (name) {
+    return Session.equals('showFirstTemplate', true);
+  };
+
+  getData = function () {
+    // when we invalidate the dataDep
+    // like with a global data context
+    // it will invalidate the inner computation
+    // of the emboxedArg causing it to rerun
+    // again. 
+    dataDep.depend();
+    return {};
+  };
+
+  Template.one.data = function () {
+    // this function gets emboxed because
+    // {{#with options}} ... {{/with}}
+    // compiles to:
+    //  UI.With(function () {
+    //    // the lookup binds the options function
+    //    // to the template's data function
+    //    // which in turn calls getData above
+    //    return Spacebars.call(self.lookup('options'));
+    //  }, UI.block(function () {
+    //    ...
+    //  });
+    //
+    //  the emboxing happens in the implementation of UI.With
+    //  which takes the function parameter and emboxes it.
+    return getData();
+  };
+
+  Template.one.helpers({
+    options: function () {
+      console.log('%cTemplate.one: options helper called', 'color: blue;');
+      return {};
+    }
+  });
+
+  Template.two.data = function () {
+    return getData();
+  };
+
+  Template.two.helpers({
+    options: function () {
+      console.log('%cTemplate.two: options helper called', 'color: green;');
+      return {};
+    }
+  });
+}
